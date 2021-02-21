21dd1ce46badb7f43749654f4bb801bc349ecff9
Martina Hytychová <martina.hyt@gmail.com>
2021-02-21 16:21:08 +0100

    ctvrty ukol hotovy

---
+++ program04.py
@@ -0,0 +1,62 @@
+morseCode = {
+    "0": "-----",
+    "1": ".----",
+    "2": "..---",
+    "3": "...--",
+    "4": "....-",
+    "5": ".....",
+    "6": "-....",
+    "7": "--...",
+    "8": "---..",
+    "9": "----.",
+    "a": ".-",
+    "b": "-...",
+    "c": "-.-.",
+    "d": "-..",
+    "e": ".",
+    "f": "..-.",
+    "g": "--.",
+    "h": "....",
+    "i": "..",
+    "j": ".---",
+    "k": "-.-",
+    "l": ".-..",
+    "m": "--",
+    "n": "-.",
+    "o": "---",
+    "p": ".--.",
+    "q": "--.-",
+    "r": ".-.",
+    "s": "...",
+    "t": "-",
+    "u": "..-",
+    "v": "...-",
+    "w": ".--",
+    "x": "-..-",
+    "y": "-.--",
+    "z": "--..",
+    ".": ".-.-.-",
+    ",": "--..--",
+    "?": "..--..",
+    "!": "-.-.--",
+    "-": "-....-",
+    "/": "-..-.",
+    "@": ".--.-.",
+    "(": "-.--.",
+    ")": "-.--.-"
+}
+
+user_text = input("Zadejte požadovaný text:")
+
+converted_text = []
+for key, value in list(morseCode.items()):
+    for element in user_text:
+        element = element.replace(" ", "/")
+        if element == key:
+            converted_text.append(value)
+
+requested_text = ""
+for letter in converted_text:
+    requested_text += letter + ", "
+
+print(requested_text)
