c8c179ae18849452d649a8e485b6c580dbb8ae6d
Martina Hytychová <martina.hyt@gmail.com>
2021-02-22 23:06:50 +0100

    oprava druhého úkolu

--- program02.py
+++ program02.py
@@ -9,19 +9,16 @@ warehouse = {
 code = input("Zadejte kód součástky:")
 amount = int(input("Zadejte počet:"))
 
-for key, value in list(warehouse.items()):
-    if code in warehouse.keys():
-        if amount > value:
-            print("Lze prodat pouze omezené množství kusů.")
-            del warehouse[code]
-            break
-        else:
-            print("Poptávku lze uspokojit v plné výši.")
-            new_amount = value - amount
-            warehouse[code] = new_amount
-            break
+if code in warehouse.keys():
+    if amount > warehouse[code]:
+        print("Lze prodat pouze omezené množství kusů.")
+        del warehouse[code]
     else:
-        print("Není skladem")
+        print("Poptávku lze uspokojit v plné výši.")
+        new_amount = warehouse[code] - amount
+        warehouse[code] = new_amount
+else:
+    print("Není skladem")
 
 print(warehouse)
 
