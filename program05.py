60466af386f3017f2fe5319e6e8225f534bd2875
Martina Hytychová <martina.hyt@gmail.com>
2021-02-24 01:39:18 +0100

    oprava pátého úkolu

--- program05.py
+++ program05.py
@@ -13,23 +13,11 @@ sales2020 = {
     "Zkus mě chytit": 6671,
 }
 
-# Vraťme se k software pro našeho nakladatele.
-# Nakladatel má nyní v software dva slovníky,
-# které obsahují informace o prodejích knih v letech 2019 a 2020.
-#
-# Uvažuj, že uživatel se zajímá o prodeje konkrétní knihy.
-# Zeptej se uživatele na název knihy a poté vypiš informaci o tom,
-# kolik se této knihy celkem prodalo.
-# Nezapomeň na to, že některé knihy byly prodávány pouze v jednom roce.
-
 book = input("Zadejte název knihy:")
 
-for key, value in list(sales2020.items()):
-    if book in sales2019.keys() and sales2020.keys():
-        print(sales2019[book] + sales2020[book])
-        break
-    elif book in sales2019.keys() or sales2020.keys():
-        print(sales2020[book] or sales2019[book])
-        break
-
-
+if book in sales2019.keys() and book in sales2020.keys():
+    print(f"Tato kniha se v roce 2019 a 2020 prodala v počtu: {sales2019[book] + sales2020[book]}")
+elif book in sales2020.keys():
+    print(f"Tato kniha se v roce 2020 prodala v počtu: {sales2020[book]}")
+else:
+    print("Taková kniha není v záznamech")
