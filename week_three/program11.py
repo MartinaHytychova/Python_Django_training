c8ecb3ab851d2801dbe03ddc7b28ef2d79b61778
Martina Hytychová <martina.hyt@gmail.com>
2021-03-06 00:19:39 +0100

    jedenáctý úkol hotový

---
+++ week_three/program11.py
@@ -0,0 +1,10 @@
+class Car:
+    def __init__(self, registration_number, brand_type, mileage):
+        self.registration_number = registration_number
+        self.brand_type = brand_type
+        self.mileage = mileage
+        self.available = True
+
+peugeot = Car("4A2 3020", "Peugeot 403 Cabrio", 47534)
+skoda = Car("1P3 4747", "Škoda Octavia", 41253)
+citroen = Car("2S3 233", "Citroen Xsara", 3452)
